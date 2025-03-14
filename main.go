aed052a601572aee40c957a03d929c395505cd06
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-13 22:16:24 -0400

    main after seperation of processes

--- main.go
+++ main.go
@@ -4,21 +4,10 @@ import (
 	"flag"
 )
 
-// scan a path to crawl it and its sub-directories
-// search for git repos
-func scan(path string) {
-	print("scan")
-}
-
-// stats to generate a graph of local git contributions
-func stats(email string) {
-	print("stats")
-}
-
 func main() {
 	var folder string
 	var email string
-	flag.StringVar(&folder, "add", "", "add a folder to scan")
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
 	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
 	flag.Parse()
 
@@ -26,6 +15,6 @@ func main() {
 		scan(folder)
 		return
 	}
-	stats(email)
 
+	stats(email)
 }
